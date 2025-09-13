69fb3702a73fe5b555d02f797d16058db73e0fed
RHBollinger1s <rhbollinger1s@gmail.com>
2025-09-13 18:34:51 -0500

    Fix variable assignment and improve root check in update and install functions

--- EZ-Bash.sh
+++ EZ-Bash.sh
@@ -18,7 +18,7 @@ END_COMMENT
 
 #----------DECLARE VARS----------
 date=$(date +%Y/%m/%d/_%H/%M/%S)
-EZBash= "EZ-Bash v1.0.0"
+EZBash="EZ-Bash v1.0.0"
 ErrorCommandNotFound="This is ez bash, a bash script to help you use the bash command line ez-ly.\nEnter command list to list basic commands"
 distro="distroNameHere"
 
@@ -42,35 +42,47 @@ echo -e "WECOME TO EZ-BASH, PLEASE ENTER COMMAND\nEnter Command list if you need
 #----------FUNCTIONS----------
 #Well, installPackage and updateSystem does check if root. This is unnessary, becase it checks if root in the start of the script, we keep it just in case somebody bypasses it...
 function updateSystem() {
-	if [[ $EUID -ne 0 ]]; then
-		echo "This command must be run as root"
-	else
-		echo -e "Updating system, please wait..."
-  		if [[ $distro == mint || $distro ==  ubuntu || $distro ==  debian || $distro == || ]]; then
-			sudo apt update && sudo apt upgrade -y
-   		elif [[ $distro == arch ]]; then
-	 		echo -e "Arch updates can break things, as it is a rolling release distro.\nWe recommend updating only once a week."
-	 		read -p "Do you want to update now? (Y/N)" answer
-				if [[ answer == Y ]]; then
-					sudo pacman -Syu
-	 			else
-	 				return
-	 	else
-	fi
- }
+  if [[ $EUID -ne 0 ]]; then
+    echo "This command must be run as root"
+    return 1
+  fi
+
+  echo -e "Updating system, please wait..."
+  if [[ "$distro" == "mint" || "$distro" == "ubuntu" || "$distro" == "debian" ]]; then
+    sudo apt update && sudo apt upgrade -y
+  elif [[ "$distro" == "arch" ]]; then
+    echo -e "Arch updates can break things, as it is a rolling release distro.\nWe recommend updating only once a week."
+    read -r -p "Do you want to update now? (Y/N) " answer
+    if [[ "$answer" == "Y" || "$answer" == "y" ]]; then
+      sudo pacman -Syu
+    else
+      return 0
+    fi
+  else
+    echo "Unsupported distro: $distro"
+  fi
+}
 function installPackage() {
-	if [[ $EUID -ne 0 ]]; then
-		echo "This command must be run as root"
-	else
-		echo -e "installing package... please wait."
-  		if [[ $distro == mint || $distro ==  ubuntu || $distro ==  debian || $distro == || ]]; then
-			echo -e "Remember, when installing packages, you should update your system.\nThe EZ-Bash command update will update your system"
-			sudo apt update && sudo apt install $1
-   		elif [[ $distro == arch ]]; then
-	 		echo -e "Remember, when installing arch packages, you should have a fairly up to date system."
-	 		sudo pacman -S $1
-	 	else
-	fi
+  if [[ $EUID -ne 0 ]]; then
+    echo "This command must be run as root"
+    return 1
+  fi
+
+  if [[ -z "$1" ]]; then
+    echo "Usage: install <package-name>"
+    return 1
+  fi
+
+  echo -e "installing package... please wait."
+  if [[ "$distro" == "mint" || "$distro" == "ubuntu" || "$distro" == "debian" ]]; then
+    echo -e "Remember, when installing packages, you should update your system.\nThe EZ-Bash command update will update your system"
+    sudo apt update && sudo apt install -y "$1"
+  elif [[ "$distro" == "arch" ]]; then
+    echo -e "Remember, when installing arch packages, you should have a fairly up to date system."
+    sudo pacman -S --noconfirm "$1"
+  else
+    echo "Unsupported distro: $distro"
+  fi
 }
 : <<'END_COMMENT'
 function changeDir() {
@@ -131,8 +143,10 @@ while true; do
 		move)
 			echo -e "move command not yet implemented"
 			;;
-		exitTo
-	 	*)
+		exitTo)
+		  break
+		  ;;
+		*)
    			echo -e "Error, command was not recognised.\nCheck spelling, or use command \"list\" to display commands."
 	esac
 done
